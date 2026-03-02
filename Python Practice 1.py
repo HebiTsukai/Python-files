624e1c38dabfb468ed502112c480de86aa4140c0
HebiTsukai <Valter_855@hotmail.com>
2026-03-01 17:47:05 -0700

    Add files via upload

---
+++ Python Practice 1.py
@@ -0,0 +1,16 @@
+def Majestic():
+    for i in range (7):
+        if i == 5: 
+            return True
+    else:
+        return False
+Mo = Majestic()
+print(Mo)
+
+-
+def MajesticRy(fPlat, lPlat):
+    print(fPlat + ", " + lPlat)
+
+MajesticRy("Bloodborne", "Final Fantasy IV")
+    
+    
